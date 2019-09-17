22180630b720a3e28ef6efbea3696e42cda09a0c
Ozzy Ozmen Celik <ozmencelik@users.noreply.github.com>
2019-09-18 01:00:55 +0300

    Initial commit

---
+++ Xml_Create_Read_Update/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace Xml_Create_Read_Update
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtQuestion = new System.Windows.Forms.TextBox();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.listboxQuestions = new System.Windows.Forms.ListBox();
+            this.listboxAnswers = new System.Windows.Forms.ListBox();
+            this.btnaddQuestion = new System.Windows.Forms.Button();
+            this.btnAddAnswer = new System.Windows.Forms.Button();
+            this.btnCreateXML = new System.Windows.Forms.Button();
+            this.BtnReadXML = new System.Windows.Forms.Button();
+            this.listBoxSelectedQuestions = new System.Windows.Forms.ListBox();
+            this.listBoxSelectedAnswers = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(124, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(252, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ADD QUESTION";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(735, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(230, 36);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ADD ANSWER";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(3, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(266, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Enter your question here...";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(597, 152);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(253, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Enter your answer here...";
+            // 
+            // txtQuestion
+            // 
+            this.txtQuestion.Location = new System.Drawing.Point(8, 214);
+            this.txtQuestion.Name = "txtQuestion";
+            this.txtQuestion.Size = new System.Drawing.Size(422, 26);
+            this.txtQuestion.TabIndex = 4;
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Location = new System.Drawing.Point(602, 214);
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.Size = new System.Drawing.Size(422, 26);
+            this.txtAnswer.TabIndex = 5;
+            // 
+            // listboxQuestions
+            // 
+            this.listboxQuestions.FormattingEnabled = true;
+            this.listboxQuestions.ItemHeight = 20;
+            this.listboxQuestions.Location = new System.Drawing.Point(8, 270);
+            this.listboxQuestions.Name = "listboxQuestions";
+            this.listboxQuestions.Size = new System.Drawing.Size(422, 584);
+            this.listboxQuestions.TabIndex = 6;
+            // 
+            // listboxAnswers
+            // 
+            this.listboxAnswers.FormattingEnabled = true;
+            this.listboxAnswers.ItemHeight = 20;
+            this.listboxAnswers.Location = new System.Drawing.Point(602, 270);
+            this.listboxAnswers.Name = "listboxAnswers";
+            this.listboxAnswers.Size = new System.Drawing.Size(422, 584);
+            this.listboxAnswers.TabIndex = 7;
+            // 
+            // btnaddQuestion
+            // 
+            this.btnaddQuestion.Location = new System.Drawing.Point(285, 138);
+            this.btnaddQuestion.Name = "btnaddQuestion";
+            this.btnaddQuestion.Size = new System.Drawing.Size(155, 56);
+            this.btnaddQuestion.TabIndex = 8;
+            this.btnaddQuestion.Text = "Add Question";
+            this.btnaddQuestion.UseVisualStyleBackColor = true;
+            this.btnaddQuestion.Click += new System.EventHandler(this.btnaddQuestion_Click);
+            // 
+            // btnAddAnswer
+            // 
+            this.btnAddAnswer.Location = new System.Drawing.Point(856, 138);
+            this.btnAddAnswer.Name = "btnAddAnswer";
+            this.btnAddAnswer.Size = new System.Drawing.Size(168, 56);
+            this.btnAddAnswer.TabIndex = 9;
+            this.btnAddAnswer.Text = "Add Answer";
+            this.btnAddAnswer.UseVisualStyleBackColor = true;
+            this.btnAddAnswer.Click += new System.EventHandler(this.btnAddAnswer_Click);
+            // 
+            // btnCreateXML
+            // 
+            this.btnCreateXML.Location = new System.Drawing.Point(448, 401);
+            this.btnCreateXML.Name = "btnCreateXML";
+            this.btnCreateXML.Size = new System.Drawing.Size(128, 162);
+            this.btnCreateXML.TabIndex = 10;
+            this.btnCreateXML.Text = "Create XML";
+            this.btnCreateXML.UseVisualStyleBackColor = true;
+            this.btnCreateXML.Click += new System.EventHandler(this.BtnXML_Click);
+            // 
+            // BtnReadXML
+            // 
+            this.BtnReadXML.Location = new System.Drawing.Point(448, 582);
+            this.BtnReadXML.Name = "BtnReadXML";
+            this.BtnReadXML.Size = new System.Drawing.Size(128, 162);
+            this.BtnReadXML.TabIndex = 11;
+            this.BtnReadXML.Text = "Read from XML";
+            this.BtnReadXML.UseVisualStyleBackColor = true;
+            this.BtnReadXML.Click += new System.EventHandler(this.BtnReadXML_Click);
+            // 
+            // listBoxSelectedQuestions
+            // 
+            this.listBoxSelectedQuestions.FormattingEnabled = true;
+            this.listBoxSelectedQuestions.ItemHeight = 20;
+            this.listBoxSelectedQuestions.Location = new System.Drawing.Point(1069, 270);
+            this.listBoxSelectedQuestions.Name = "listBoxSelectedQuestions";
+            this.listBoxSelectedQuestions.Size = new System.Drawing.Size(333, 584);
+            this.listBoxSelectedQuestions.TabIndex = 12;
+            // 
+            // listBoxSelectedAnswers
+            // 
+            this.listBoxSelectedAnswers.FormattingEnabled = true;
+            this.listBoxSelectedAnswers.ItemHeight = 20;
+            this.listBoxSelectedAnswers.Location = new System.Drawing.Point(1434, 270);
+            this.listBoxSelectedAnswers.Name = "listBoxSelectedAnswers";
+            this.listBoxSelectedAnswers.Size = new System.Drawing.Size(333, 584);
+            this.listBoxSelectedAnswers.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(1076, 199);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(201, 25);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Selected Questions";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(1443, 199);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(186, 25);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Selected Answers";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(1271, 56);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(429, 36);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "SELECTED INFORMATIONS";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1774, 869);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.listBoxSelectedAnswers);
+            this.Controls.Add(this.listBoxSelectedQuestions);
+            this.Controls.Add(this.BtnReadXML);
+            this.Controls.Add(this.btnCreateXML);
+            this.Controls.Add(this.btnAddAnswer);
+            this.Controls.Add(this.btnaddQuestion);
+            this.Controls.Add(this.listboxAnswers);
+            this.Controls.Add(this.listboxQuestions);
+            this.Controls.Add(this.txtAnswer);
+            this.Controls.Add(this.txtQuestion);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtQuestion;
+        private System.Windows.Forms.TextBox txtAnswer;
+        private System.Windows.Forms.ListBox listboxQuestions;
+        private System.Windows.Forms.ListBox listboxAnswers;
+        private System.Windows.Forms.Button btnaddQuestion;
+        private System.Windows.Forms.Button btnAddAnswer;
+        private System.Windows.Forms.Button btnCreateXML;
+        private System.Windows.Forms.Button BtnReadXML;
+        private System.Windows.Forms.ListBox listBoxSelectedQuestions;
+        private System.Windows.Forms.ListBox listBoxSelectedAnswers;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Xml_Create_Read_Update/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Xml_Create_Read_Update
+{
+    public partial class Form1 : Form
+    {
+
+        /*This tuttorial created by Ozzy Ozmen Celik for education.
+          And puspose of this tuttorial is showing how to create & update XML File Also how to read from xml file  */
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+        // Adding question into Listbox by textboxQuestion
+        private void btnaddQuestion_Click(object sender, EventArgs e)
+        {
+            listboxQuestions.Items.Add(txtQuestion.Text);
+            txtQuestion.Clear();
+            txtQuestion.Focus();
+        }
+
+        // Adding answer into Listbox by textboxAnswer
+        private void btnAddAnswer_Click(object sender, EventArgs e)
+        {
+            listboxAnswers.Items.Add(txtAnswer.Text);
+            txtAnswer.Clear();
+            txtAnswer.Focus();
+        }
+
+        // Create XmL button event
+        private void BtnXML_Click(object sender, EventArgs e)
+        {
+
+            if (listboxQuestions.Items.Count > 0 && listboxAnswers.Items.Count > 0)
+            {
+                XMLCreat();
+                listboxQuestions.Items.Clear();
+                listboxAnswers.Items.Clear();
+
+            }
+            else
+            {
+            
+                MessageBox.Show("Upps XML File couldnt create cause of you forgot adding question and answer both !!!");
+            }
+        }
+
+
+
+        // creating a xml file
+        private void XMLCreat()
+        {
+            // created a xmltextwriter method and added xmlfile with set encoding type
+            XmlTextWriter xmlcrt = new XmlTextWriter("Questions.xml", Encoding.Unicode);
+
+            // set first element of xml file which one will be included child elements inside.
+            xmlcrt.WriteStartElement("Questions");
+
+            // Check the file directory if our xml file already created or not
+            string File_Directory = AppDomain.CurrentDomain.BaseDirectory.ToString() + "Questions.xml";
+
+            if (File.Exists(File_Directory) == true) 
+            {
+                // created a string variable ( questionstring ) to set to Selected Questions Listbox.selecteditem.
+                // And set child elements
+                // end the element
+
+                for (int i = 0; i < listBoxSelectedQuestions.Items.Count; i++)
+                {
+                    string questionstring = listBoxSelectedQuestions.Items[i].ToString();
+                    xmlcrt.WriteStartElement("Question");
+                    xmlcrt.WriteElementString("QuestionSentence", questionstring);
+                    xmlcrt.WriteEndElement();
+                }
+
+                // created a string variable ( answerstring ) to set to Selected Answers Listbox.selecteditem.
+                // And set child elements
+                // end the element
+                for (int i = 0; i < listBoxSelectedAnswers.Items.Count; i++)
+                {
+                    string answerstring = listBoxSelectedAnswers.Items[i].ToString();
+                    xmlcrt.WriteStartElement("Answer");
+                    xmlcrt.WriteElementString("AnswerSentence", answerstring);
+                    xmlcrt.WriteEndElement();
+                }
+            }
+
+            // created a string variable ( questionstring ) to set to Questions Listbox.selecteditem.
+            // And set child elements
+            // end the element
+
+            for (int i = 0; i < listboxQuestions.Items.Count; i++)
+            {
+
+                string questionstring = listboxQuestions.Items[i].ToString();
+                xmlcrt.WriteStartElement("Question");
+                xmlcrt.WriteElementString("QuestionSentence", questionstring);
+                xmlcrt.WriteEndElement();
+
+            }
+            // created a string variable ( answerstring ) to set to Answer Listbox.selecteditem.
+            // And set child elements
+            // end the element
+
+            for (int j = 0; j < listboxAnswers.Items.Count; j++)
+            {
+                string answerstring = listboxAnswers.Items[j].ToString();
+
+                xmlcrt.WriteStartElement("Answer");
+                xmlcrt.WriteElementString("AnswerSentence", answerstring);
+                xmlcrt.WriteEndElement();
+            }
+
+            // end the element
+            xmlcrt.WriteEndElement();
+
+            // created the xml fie with close event.
+            xmlcrt.Close();
+       
+           
+
+        }
+
+        // Read questions from  xml file
+        private void XmlReadQuestions()
+        {
+            //created xmldokument method
+            XmlDocument readxml = new XmlDocument();
+
+            // loaded the file 
+            readxml.Load("Questions.xml");
+
+            // created Questions object as XmlNode and set it wth questions
+            XmlNode Questions = readxml.SelectSingleNode("Questions");
+
+            // called all questionSentences each by each with foreach
+            foreach (XmlNode question in Questions.SelectNodes("Question"))
+            {
+                //created aa string and set it with QuestionSentence
+                string aa = question.SelectSingleNode("QuestionSentence").InnerText;
+
+               // and added in to listbox
+                listBoxSelectedQuestions.Items.Add(aa);
+
+                //created array named s.q
+                string[] SelectedQuestions = new string[listBoxSelectedQuestions.Items.Count];
+
+                // put values in to s.q array with for
+                for (int i = 0; i < listBoxSelectedQuestions.Items.Count; i++)
+                {
+                  SelectedQuestions[i] = listBoxSelectedQuestions.Items[i].ToString();
+
+                   
+                }
+            }
+
+        }
+        // Read answers from xml file
+        private void XmlReadAnsers()
+        {
+            //created xmldokument method
+            XmlDocument readxml = new XmlDocument();
+
+            // loaded the file
+            readxml.Load("Questions.xml");
+
+            // created Questions object as XmlNode and set it wth questions
+            XmlNode Questions = readxml.SelectSingleNode("Questions");
+
+            // called all answernSentences each by each with foreach
+            foreach (XmlNode answer in Questions.SelectNodes("Answer"))
+            {
+                //created aa string and set it with AnswerSentence
+                string aa = answer.SelectSingleNode("AnswerSentence").InnerText;
+
+                // called all questionSentences each by each with foreach
+                listBoxSelectedAnswers.Items.Add(aa);
+
+                //created array named s.a
+                string[] SelectedAnswers = new string[listBoxSelectedAnswers.Items.Count];
+
+                // put values in to s.a array with for
+                for (int i = 0; i < listBoxSelectedAnswers.Items.Count; i++)
+                {
+                    SelectedAnswers[i] = listBoxSelectedAnswers.Items[i].ToString();
+
+
+                }
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           // i didnt use forum load but u can use if wantto set something work on load event
+        }
+
+        //read from xml button event
+        private void BtnReadXML_Click(object sender, EventArgs e)
+        {
+            listBoxSelectedQuestions.Items.Clear();
+            listBoxSelectedAnswers.Items.Clear();
+
+            // Check the file directory if our xml file already created or not
+            string File_Directory = AppDomain.CurrentDomain.BaseDirectory.ToString() + "Questions.xml";
+
+            if (File.Exists(File_Directory) == true) 
+            {
+                //called our read questions and answers methods
+                XmlReadQuestions();
+                XmlReadAnsers();
+            }
+            else
+            {
+                MessageBox.Show("Sorry there must be a xml file in the project before update and read actions !!!");
+                MessageBox.Show("PLEASE CREATE NEW XML FILE !!!");
+            }
+           
+            
+        }
+    }
+}
